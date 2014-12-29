b6131cc6b1eeff360aa5a67412d835178d408017
Pedro Reys <pedroreys@gmail.com>
2014-12-29 13:00:26 -0200

    First example using Default Convention

---
+++ src/FixieCalculator.Tests/CalculatorTests.cs
@@ -0,0 +1,69 @@
+﻿namespace FixieCalculator.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+    using Shouldly;
+
+    public class CalculatorTests
+    {
+        private readonly Calculator _calculator;
+
+        public CalculatorTests()
+        {
+            _calculator = new Calculator();
+        }
+
+        public void Should_add_two_numbers()
+        {
+            var result = _calculator.Add(1, 2);
+            result.ShouldBe(3);
+        }
+
+        public void Should_subract_two_numbers()
+        {
+            var result = _calculator.Subtract(10, 4);
+            result.ShouldBe(6);
+        }
+
+        public void Should_multiply_two_numbers()
+        {
+            var result = _calculator.Multiply(2, 4);
+            result.ShouldBe(8);
+        }
+
+        public void Should_divide_two_numbers()
+        {
+            var result = _calculator.Divide(10,2);
+            result.ShouldBe(5);
+        }
+
+        public async Task Should_add_two_numbers_async()
+        {
+            var result = await _calculator.AddAsync(4, 5);
+            result.ShouldBe(9);
+        }
+
+        private void Should_not_run_private_methods()
+        {
+            throw new Exception("Fixie Shouldn't run private methods");
+        }
+
+        private int Should_not_run_non_void_methods()
+        {
+            throw new Exception("Fixie shouldn't run non-void methods");
+        }
+    }
+
+    public class WrongTestFixtureName
+    {
+        public WrongTestFixtureName()
+        {
+            throw new Exception("Fixie shouldn't instanciate classes that it doesn't match the test class name convention");
+        }
+
+        public void Should_not_run_this_test()
+        {
+            throw new Exception("Fixie shouldn't run this test");
+        }
+    }
+}
---
+++ src/FixieCalculator/Calculator.cs
@@ -0,0 +1,32 @@
+﻿namespace FixieCalculator
+{
+    using System.Threading.Tasks;
+
+    public class Calculator
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int Multiply(int x, int y)
+        {
+            return x*y;
+        }
+
+        public int Divide(int x, int y)
+        {
+            return x/y;
+        }
+
+        public Task<int> AddAsync(int x, int y)
+        {
+            return Task.Run(() => x + y);
+        } 
+    }
+}
